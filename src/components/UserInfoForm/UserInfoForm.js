aac0700a837aa3438bb55019ec46b75c1791cec0
eklipsorz <eklipsorz@protonmail.ch>
2022-09-06 00:28:51 +0800

    feat: add UserInfoForm

--- src/components/UserInfoForm/UserInfoForm.js
+++ src/components/UserInfoForm/UserInfoForm.js
@@ -1,7 +1,25 @@
 import Card from '../UI/Card';
+import Button from '../UI/Button';
+import styles from './UserInfoForm.module.css';
 
 const UserInfoForm = (props) => {
-  return <Card></Card>;
+  return (
+    <Card>
+      <form>
+        <div className={styles['form-control']}>
+          <label>Username</label>
+          <input />
+        </div>
+        <div className={styles['form-control']}>
+          <label>Age(Years)</label>
+          <input />
+        </div>
+        <Button type='submit' onClick>
+          Add User
+        </Button>
+      </form>
+    </Card>
+  );
 };
 
 export default UserInfoForm;
