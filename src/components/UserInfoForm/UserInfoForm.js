805ac4a3b979b85f858780bb80818220bec00772
eklipsorz <eklipsorz@protonmail.ch>
2022-09-05 23:35:03 +0800

    feat: add draft version for UserInfoForm & UserInfoList

---
+++ src/components/UserInfoForm/UserInfoForm.js
@@ -0,0 +1,7 @@
+import Card from '../UI/Card';
+
+const UserInfoForm = (props) => {
+  return <Card></Card>;
+};
+
+export default UserInfoForm;
---
+++ src/components/UserInfoList/UserInfoList.js
@@ -0,0 +1,7 @@
+import Card from '../UI/Card';
+
+const UserInfoList = (props) => {
+  return <Card></Card>;
+};
+
+export default UserInfoList;
